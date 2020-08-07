35be921df70a93b602a2894d3099244a2eec761b
kacper38 <57570120+kacper38@users.noreply.github.com>
2020-08-07 12:20:32 +0200

    Update largest_palindrome.py

--- largest_palindrome.py
+++ largest_palindrome.py
@@ -1,5 +1,5 @@
 def largest_palindrome_finder():
     lpp = max([x*y for x in range(100,1000) \
-           for y in range(100,1000) \
+           for y in range(x,1000) \
            if str(x*y) == str(x*y)[::-1]])
     return lpp
